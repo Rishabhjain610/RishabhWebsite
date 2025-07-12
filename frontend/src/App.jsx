f0f3fed936f8a6f1ab25248a36774afcabf5af62
Rishabh Jain <jainrishabh2610@gmail.com>
2025-07-12 21:20:15 +0530

    updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,25 +10,28 @@ import Skills from "./componenets/Skills";
 import Projects from "./componenets/Projects";
 import Experience from "./componenets/Experience";
 import Contact from "./componenets/Contact";
-
+import { motion, useScroll } from "framer-motion";
 const App = () => {
- const [loading, setLoading] = useState(true);
-
+  const [loading, setLoading] = useState(true);
+  const scrollYprogress = useScroll().scrollYProgress;
   useEffect(() => {
     // Simulate loading time (replace with your real loading logic)
     const timer = setTimeout(() => setLoading(false), 3000);
     return () => clearTimeout(timer);
   }, []);
-
   if (loading) {
     return <Preloader />;
   }
 
   return (
     <>
-    
       <div className="min-h-[600vh] text-white overflow-hidden">
         <Bg />
+        <motion.div
+          className="h-[3px] w-full bg-purple-500 fixed  left-0 z-50 origin-left shadow-2xl drop-shadow-purple-500 rounded-full"
+          style={{ scaleX: scrollYprogress }}
+          initial={{ scaleX: 0 }}
+        ></motion.div>
 
         <Navbar />
         <Hero />
@@ -37,7 +40,6 @@ const App = () => {
         <Projects />
         <Experience />
         <Contact />
-      
       </div>
     </>
   );
